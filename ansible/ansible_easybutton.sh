55a7f9faca4f1305ffa78ae134cf6c30785cf0c9
ec2-user <ec2-user@ip-10-11-2-108.nrel.gov>
2015-10-26 18:01:26 -0400

    Working on ansible support for RedHat.  - Updated ansible_easybutton to support RHEL 6  - Switched from ubuntu-check role to more generic os-check role  - Updated os-check, zmq, firewall, geoipupdate and elasticsearch roles to work with RHEL 6  - Added epel role to install epel repository on RHEL 6

--- ansible/ansible_easybutton.sh
+++ ansible/ansible_easybutton.sh
@@ -19,12 +19,17 @@ if [ -f /etc/lsb-release ]; then
     . /etc/lsb-release
     OS=$DISTRIB_ID
     VER=$DISTRIB_RELEASE
+    if [ -z $OS ]; then
+        # RedHat LSB doesn't contain all the info in /etc/lsb-release
+        OS=`lsb_release -i | cut -s -f 2`
+        VER=`lsb_release -r | cut -s -f2 | cut -d. -f 1`
+    fi
 elif [ -f /etc/debian_version ]; then
-    OS=Debian  # XXX or Ubuntu??
+    OS="Debian"  # XXX or Ubuntu??
     VER=$(cat /etc/debian_version)
 elif [ -f /etc/redhat-release ]; then
-    # TODO add code for Red Hat and CentOS here
-    ...
+    OS="RedHatEnterpriseServer"
+    VER=`cat /etc/redhat-release |sed -r -e 's/.*release //' -e 's/\..*//'`
 else
     OS=$(uname -s)
     VER=$(uname -r)
@@ -48,8 +53,26 @@ case $OS in
     "Darwin" )
         echo 'Darwin not yet supported...' ;;
 
-    "Redhat" )
-        echo 'Redhat not yet supported...' ;;
+    "Redhat"|"RedHatEnterpriseServer" )
+        if [ $VER == "6" ]; then
+            yum -y update && yum install -y wget git && yum -y --setopt=group_package_types=mandatory groupinstall "Development Tools"
+            if [ ! -f /etc/yum.repos.d/epel.repo ]; then
+                wget "https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm";
+                yum -y install ./epel-release-latest-6.noarch.rpm
+            fi
+            yum -y erase python-crypto && yum -y install python-devel python-pip python-httplib2 python-jinja2 python-netaddr python-setuptools PyYAML
+            pip install ansible
+            if [ -d massive-octo-spice ]; then
+                (cd massive-octo-spice && git checkout $BRANCH && git pull)
+            else
+                git clone https://github.com/csirtgadgets/massive-octo-spice.git -b $BRANCH
+            fi
+            cd massive-octo-spice/ansible
+            ansible-playbook localhost.yml
+        else
+            echo "RedHat version $VER is not supported yet..."
+        fi
+        ;;
 
     "CentOS" )
         echo 'CentOS not yet supported...' ;;
